34e9ad2fe6ce06c07db031d6d87d99dd858e8931
roxannecvl <roxanne.chevalley03@gmail.com>
2024-03-22 12:44:54 +0100

    feat : used UInputMenu Component from NuxtUI in index.vue (#16) This component creates a DropDown Menu to choose a celebrity, it uses the function getAutocompleteSuggestion to choose the options in the Dropdown menu.

--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,30 @@
     Home
   </div>
   <div class="font-medium text-blue-600 dark:text-blue-500 hover:underline">
-    <ULink
-        to="/example">
-      Go to example
-    </ULink>
+    <ULink to="/example">Go to example</ULink>
   </div>
-
+  <UInputMenu
+      v-model="selectedName"
+      :search="getAutocompleteSuggestions"
+      placeholder="Enter a name..."
+      option-attribute="name"
+      trailing
+      by="id"
+  />
+  <UCard class="m-4">{{ intro }}</UCard>
 </template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+import { getAutocompleteSuggestions} from "~/model/CelebrityList";
+
+
+const selectedName = ref("");
+const intro = ref("");
+
+const getData = async () => {
+  intro.value = "data"
+}
+
+watch(selectedName, getData)
+</script>
