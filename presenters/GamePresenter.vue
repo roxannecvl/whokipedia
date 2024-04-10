03e2b00163c371c7c6ffad6f9a797cd497f46e45
roxannecvl <roxanne.chevalley03@gmail.com>
2024-04-10 14:46:10 +0200

    fix : forgot to add presenter in last commit (#69)

--- presenters/GamePresenter.vue
+++ presenters/GamePresenter.vue
@@ -12,14 +12,35 @@ defineProps({
 })
 
 const baseString = "https://en.wikipedia.org/wiki/";
+const validGuess = ref(true);
+const toast = useToast();
+const errTitle = "Already guessed!"
+const errDescription = "This guess doesn't count as a new guess, try again :)"
+
+function guessAndCheck(name : string, model : GameStore){
+  validGuess.value = model.makeAGuess.bind(model)(name);
+  if(!validGuess.value) {
+    toast.add({
+      title: errTitle,
+      description: errDescription,
+      icon: 'i-heroicons-x-circle',
+      color:"red",
+      timeout: 2500
+    })
+
+    setTimeout(() => {
+      validGuess.value = true;
+    }, 2500);
+  }
+}
 
 </script>
 
 <template>
   <div class="flex flex-row">
     <GameCenterView
-        @new-name-set="selectedName => model.makeAGuess.bind(model)(selectedName)"
-        :intro="model.intro" :over="model.end" :name="model.name" :win = "model.win"
+        @new-name-set="selectedName => guessAndCheck(selectedName, model)"
+        :intro="model.intro" :over="model.end" :name="model.name" :win = "model.win" :redBackground="!validGuess"
     />
     <InfoboxView
         :fields = "model.infobox" :imageUrl="model.imageUrl" :blur="model.blur"
