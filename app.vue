56653e6048ae79ff49c7baaafbb33508a24074be
Jean Perbet <jeanperbet@icloud.com>
2024-04-17 16:56:32 +0200

    refactor: simplified app.vue (#88)

--- app.vue
+++ app.vue
@@ -18,19 +18,14 @@ const user = useCurrentUser()
 </script>
 
 <template>
-  <Body
-      class="bg-gray-50 dark:bg-gray-950 h-full">
-  <UContainer
-      class="p-0 sm:py-5">
-    <UCard :ui="{
-      rounded: 'rounded-none sm:rounded-lg',
-    }">
+  <Body class="bg-gray-50 dark:bg-gray-950 h-full">
+  <UContainer class="p-0 sm:py-5">
+    <UCard :ui="{ rounded: 'rounded-none sm:rounded-lg' }">
       <template #header>
         <div class="flex justify-between items-center">
           <div>
             <ClientOnly>
-              <UTooltip
-                  :text="`Switch to ${isDark ? 'Light' : 'Dark'} Mode`">
+              <UTooltip :text="`Switch to ${isDark ? 'Light' : 'Dark'} Mode`">
                 <UButton
                     :icon="isDark ? 'i-heroicons-moon-solid' : 'i-heroicons-sun-solid'"
                     @click="isDark = !isDark"
@@ -40,12 +35,8 @@ const user = useCurrentUser()
           </div>
           <a href="/">
             <div class="flex">
-              <UIcon
-                  name="i-heroicons-magnifying-glass-16-solid"
-                  class="text-primary w-10 h-10 hidden sm:block"/>
-              <p class="font-black text-3xl hidden sm:block select-none">
-                Whokipedia
-              </p>
+              <UIcon name="i-heroicons-magnifying-glass-16-solid" class="text-primary w-10 h-10 hidden sm:block"/>
+              <p class="font-black text-3xl hidden sm:block select-none">Whokipedia</p>
             </div>
           </a>
           <div class="flex justify-between items-center">
@@ -57,8 +48,7 @@ const user = useCurrentUser()
             </div>
             <div class="p-3">
               <ClientOnly>
-                <UTooltip
-                    text="Open on Github">
+                <UTooltip text="Open on Github">
                   <UButton
                       :to="`https://github.com/roxannecvl/whokipedia`"
                       icon="i-simple-icons-github"
