fa54f9c29baad12beebba4f9ce7ed14dc51b1aee
Jean Perbet <jeanperbet@icloud.com>
2024-04-17 16:08:34 +0200

    fix: removed useless middleware (#88)

--- middleware/auth.ts
+++
@@ -1,8 +0,0 @@
-/**
- * This middleware is used for pages which depend on user authentication, e.g. their content differ if user
- * is authenticated or not. When using this middleware, a page is sure that authentication state will be retrieved
- * prior to page display.
- */
-export default defineNuxtRouteMiddleware(async () => {
-    const user = await getCurrentUser()
-})
