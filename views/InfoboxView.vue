fed2c0a55416ed252d3ad7273417d0d6ef522d60
Martin Catheland <m.catheland@gmail.com>
2024-04-26 18:06:16 +0200

    feat: add animation on revealing less blurred image (#98)

--- views/InfoboxView.vue
+++ views/InfoboxView.vue
@@ -1,3 +1,4 @@
+<!--suppress ALL -->
 <script setup lang="ts">
 import { type InfoboxHint, compulsoryLabels } from "~/model/Hint"
 import { capitalize } from "~/utilities/Utils"
@@ -28,7 +29,9 @@ const props = defineProps( {
     <UCard class="flex flex-col items-center justify-center" :ui="{header: {padding:''}}">
       <template #header>
         <div class="flex flex-col items-center justify-center my-5">
-          <img :src="imageUrl" alt="image" class="w-40 object-cover pointer-events-none rounded-md rounded-b-md shadow-md"/>
+          <transition name="scale-up" mode="out-in">
+            <img :src="imageUrl" :key="imageUrl" alt="image" class="w-40 object-cover pointer-events-none rounded-md rounded-b-md shadow-md"/>
+          </transition>
         </div>
       </template>
       <div class="flex flex-col items-center justify-center py-5">
@@ -61,12 +64,13 @@ const props = defineProps( {
   </div>
 </template>
 
+
 <style scoped>
 .v-enter-active {
-  animation: scale 1.5s 
+  animation: scale-text 1.2s
 }
 
-@keyframes scale {
+@keyframes scale-text {
   0% {
     transform: scale(1)
   }
@@ -79,6 +83,19 @@ const props = defineProps( {
   100% {
     transform: scale(1)
   }
+}
+
+.scale-up-enter-active {
+  transition: all 1.2s;
+}
+
+.scale-up-leave-active {
+  transition: all 0.6s;
+}
+
+.scale-up-enter-from, .scale-up-leave-to {
+  transform: scale(1.05);
+  box-shadow: 0 0 15px 6px rgb(245, 158, 12, 0.9);
 }
 
 </style>
